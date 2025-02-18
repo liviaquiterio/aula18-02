64fd21c83a51cd5f1a7e7095e03da2aaabde019e
liviaquiterio <127116102+liviaquiterio@users.noreply.github.com>
2025-02-18 07:48:55 -0300

    Add files via upload

---
+++ media3numeros.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Média de números</title>
+</head>
+<body>
+    <form action="mediaexibir3numero.php" method="post">
+    <label>Número 1:</label>
+    <input type="text" name="numero1" size="100" maxlength="50" required><br>
+    <label>Número 2:</label>
+    <input type="text" name="numero2" size="100" maxlength="50" required><br>
+    <label>Número 3:</label>
+    <input type="text" name="numero3" size="100" maxlength="50" required><br>
+    <label>Calcular</label>
+    <input type="submit" name="calcular" size="100" maxlength="50" required><br>
+    </form>
+</body>
+</html>
---
+++ mediaexibir3numero.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $numero1 = $_POST["numero1"];
+    $numero2 = $_POST["numero2"];
+    $numero3 = $_POST["numero3"];
+    $total = $numero1 + $numero2 + $numero3;
+    echo "A média é: ", $total
+    ?>
+</body>
+</html>
