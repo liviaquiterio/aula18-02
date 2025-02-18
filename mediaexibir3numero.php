deb7f5f74940dafec6d7d12cc491b7519cda6e7a
liviaquiterio <127116102+liviaquiterio@users.noreply.github.com>
2025-02-18 07:54:48 -0300

    Add files via upload

--- mediaexibir3numero.php
+++ mediaexibir3numero.php
@@ -10,7 +10,7 @@
     $numero1 = $_POST["numero1"];
     $numero2 = $_POST["numero2"];
     $numero3 = $_POST["numero3"];
-    $total = $numero1 + $numero2 + $numero3;
+    $total = ($numero1 + $numero2 + $numero3)/3;
     echo "A média é: ", $total
     ?>
 </body>
